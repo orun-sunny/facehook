dbc81e8d956725476d4bd8f4f8d95c4fad9408d7
orun sunny <sunny.mukut@gmail.com>
2024-03-12 16:03:26 +0600

    error

---
+++ src/components/posts/PostList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import PostCard from "./PostCard";
+const PostList = ({ posts }) => {
+  return !!posts && posts.map((post) => <PostCard key={post.id} post={post} />);
+};
+
+export default PostList;
